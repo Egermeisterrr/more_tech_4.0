b143b1fdc241fcc37c103abfefdc097ef97dfaab
Egor Fokin <egermeister03@gmail.com>
2022-10-07 18:17:34 +0300

    hilt

--- app/src/main/java/com/example/moretech40android/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/moretech40android/presentation/activity/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.moretech40android
+package com.example.moretech40android.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moretech40android.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
