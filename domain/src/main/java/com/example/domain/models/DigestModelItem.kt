9a346e5dab4f2da1029bf8b0288a9772a40f7df0
Egor Fokin <egermeister03@gmail.com>
2022-10-09 00:30:30 +0300

    swipe refresh layout

---
+++ domain/src/main/java/com/example/domain/models/DigestModelItem.kt
@@ -0,0 +1,11 @@
+package com.example.domain.models
+
+data class DigestModelItem(
+    val clean_text_list_lem: List<String>,
+    val img: String,
+    val tags: List<String>,
+    val text: String,
+    val text_clean: String,
+    val time: String,
+    val title: String
+)
